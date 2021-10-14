facb828f4b9640dd266d0249713aa838225bc221
elsa <elsa_ase@yahoo.com>
2021-10-14 18:41:54 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Headre from './components/Header'
+import "./App.css";
+//function App() {
+ // const name = 'Ezue'
+//  return (
+ //   <div className="App">
+ //      <h1>Hello From React</h1>
+      
+////       </div>
+
+//  )
+//}
+
+
+class App extends React.Component {
+  render() {
+    return  <div className="App">
+      <h1>React Assignment 1</h1>
+   <p>Please interact in the class- it is good for all of us </p>
+   </div>;
+  }
+}
+export default App
